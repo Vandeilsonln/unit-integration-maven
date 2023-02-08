a4bb0a5dc50bbaa51290ac568e15937f44e01d76
Vandeilsonpp <vandeilson.nobre@picpay.com>
2023-02-07 21:14:32 -0300

    initial commit

---
+++ src/main/kotlin/com/vaneilson/itut/ItutApplication.kt
@@ -0,0 +1,11 @@
+package com.vaneilson.itut
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ItutApplication
+
+fun main(args: Array<String>) {
+	runApplication<ItutApplication>(*args)
+}
---
+++ src/test/kotlin/com/vaneilson/itut/ItutApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.vaneilson.itut
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ItutApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
